4e90a74d6104ab39cb16a126c09460488611122e
yuebanyishenqiu <thisispwj@outlook.com>
2017-12-10 16:21:33 +0800

    adjust folder

--- src/spider.py
+++ src/spider.py
@@ -1,12 +1,13 @@
-#This is a tiny spider to download the news form soho
-#To do this job, you should have requests and BeautifulSoup installed
-#Python2 or Python3 does not matter, just some syntax differences
-#Enjoy your coding : )
 #Author: yuebanyishenqiu
 #E-mail:thisispwj@outlook.com
 # -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf8")
 import requests
 from bs4 import BeautifulSoup
+import codecs
+import time
 def get_request_kwargs(timeout,useragent):
     #add some parameters
     return{
@@ -16,12 +17,11 @@ def get_request_kwargs(timeout,useragent):
     }
 
 def getHtml(url):
-    #The code below is from the library of newspaper, FAIL_CODING seems only occurs in some non-text website
     FAIL_ENCODING = 'ISO-8859-1'
     useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36'
     timeout = 7
     try:
-        print (url)
+        print url
         response=requests.get(url,**get_request_kwargs(timeout,useragent))
         if response.encoding!=FAIL_ENCODING:
             html = response.text
@@ -41,14 +41,10 @@ def getSoup(url):
 def getSubUrls(url):
     soup=getSoup(url)
     if soup is None:
-        print ("Error")
+        print "Error"
         return 
-    #suburl has a class attribute named "news-item"
-    #btw, some url to ads have a {"data-god-id":re.compile("\d+")} attribute, does this mean customers are god??: ) funnyXDDD
     tags=[x for x in soup.find_all(attrs={"data-role":"news-item"})]#if x not in soup.find_all(attrs={"data-god-id":re.compile("\d+")})]
     for tag in tags:
-        #links=tag.find('h4')
-        #if not links:continue
         child=tag.select("h4 > a")
         link=child[0].get('href')
         if link is not None:
@@ -56,14 +52,19 @@ def getSubUrls(url):
                 link="http:"+link
             yield link
 def spider(url):
+    i = 0
     for suburl in getSubUrls(url):
         soup=getSoup(suburl)
         title_tag = soup.find('h1')
+        t_stamp = time.time()
+        st = time.localtime(t_stamp)
+        t_str = time.strftime("%Y-%m-%d",st)
         for title in title_tag.stripped_strings:
-            f = open('./info/' + title + '.txt','w')
+            f = codecs.open('../data/' + t_str + "_" + str(i),'w',encoding="utf8")
         article = soup.find('article')
         for para in article.stripped_strings:
             f.writelines(para+'\n')
         f.close()
+        i += 1
 if __name__=="__main__":
     spider('http://business.sohu.com/')
